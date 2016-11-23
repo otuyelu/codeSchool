41d8b5777b27ca8141cc915ff4781b5788b809f2
otuyelu <jotuyelu@yahoo.com>
2016-11-23 07:39:56 -0500

    Update hooverdam.js

--- hooverdam.js
+++ hooverdam.js
@@ -6,8 +6,8 @@ when only the even numbered turbines are turned on. And they want it all in just
 */
 var totalGen = 19;
 var totalMW = 0;
-for (var generator = 1;turbine <= totalGen; generator++){
-  if(generator % 2 == 0){
+for (var generator = 1;generator <= totalGen; generator++){
+  if(generator % 2 === 0){
     if(generator < 5){
       totalMW += 62;
       console.log("Generator #"+generator+" is on, adding 62 MW, for a total of "+totalMW+" MW!");
