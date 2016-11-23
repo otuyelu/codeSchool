08a41d407f8f3d1329ca76e0c37159cfbf71eed0
otuyelu <jotuyelu@yahoo.com>
2016-11-23 08:27:18 -0500

    add loop to prompt until user age is confirmed

--- userProfile.js
+++ userProfile.js
@@ -1,6 +1,14 @@
 /*
 With the userAge variable, we can ensure that the user has entered their age correctly. 
-Use a pop-up window function to confirm the user entry
+Use a pop-up window function to confirm the user entry:
+Create a while loop that will continue to execute as long as the user has not entered the correct age. 
+Inside the loop, provide a confirmation that the age has been entered correctly:
 */
-var userAge = prompt("What's your age, user?");
-var ageIsCorrect = confirm("You entered "+userAge+". Is this correct?");
+var ageIsCorrect = false;
+while(ageIsCorrect == false){
+  var userAge = prompt("What's your age, user?");
+  if(confirm("You entered "+userAge+". Is this correct?")){
+    ageIsCorrect = true;
+    alert("Great! Your age is logged as "+userAge+".");
+  }
+}
