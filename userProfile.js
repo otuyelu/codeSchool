beebe6be8481496e472796ac3436c8bf63ba4c67
otuyelu <jotuyelu@yahoo.com>
2016-11-23 07:58:39 -0500

    Create userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,6 @@
+/*
+With the userAge variable, we can ensure that the user has entered their age correctly. 
+Use a pop-up window function to confirm the user entry
+*/
+var userAge = prompt("What's your age, user?");
+var ageIsCorrect = confirm("You entered "+userAge+". Is this correct?");
